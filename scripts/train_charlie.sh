13dccf2aa8d19dee97d83accab7d41147ec2491f
Xiaoyao-Li <puhaoli01@gmail.com>
2023-12-22 13:48:20 +0800

    update

--- scripts/train_charlie.sh
+++ scripts/train_charlie.sh
@@ -9,4 +9,6 @@ export CUDA_VISIBLE_DEVICES=${GPU}
 CHARLIE=1 python train.py hydra/job_logging=none hydra/hydra_logging=none \
                  exp_name=${EXP_NAME} \
                  model=${MODEL} \
-                 task=${TASK}
+                 task=${TASK} \
+                 model.enable_image=true \
+                 model.enable_hint=true
